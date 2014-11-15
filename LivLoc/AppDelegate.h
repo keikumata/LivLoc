60bad696b1f0b61584722f9ac5ab5c59a65cd8f4
Kei Yoshikoshi <keiyoshikoshi@Keis-MBP.wireless.duke.local>
2014-11-15 13:09:46 -0500

    Initial Commit

---
+++ LivLoc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LivLoc
+//
+//  Created by Kei Yoshikoshi on 11/15/14.
+//  Copyright (c) 2014 LivLoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LivLoc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LivLoc
+//
+//  Created by Kei Yoshikoshi on 11/15/14.
+//  Copyright (c) 2014 LivLoc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
